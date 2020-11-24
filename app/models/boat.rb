16ce29ab9122a33bcb001fac09a67d6c4dddac0c
maxime jourdan <maximejourdan1@gmail.com>
2020-11-24 11:31:41 +0100

    set up cloudinary

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,6 +1,7 @@
 class Boat < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+  has_one_attached :photo
 
   validates :title, presence: true
   validates :description, presence: true
