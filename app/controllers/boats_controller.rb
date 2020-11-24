735ee6b05ff6fcf39322eb51ab09f77c8d339a42
maxime jourdan <maximejourdan1@gmail.com>
2020-11-24 18:36:13 +0100

    Add new form boat

---
+++ app/controllers/boats_controller.rb
@@ -0,0 +1,23 @@
+class BoatsController < ApplicationController
+
+  def new
+    @boat = Boat.new
+  end
+
+  def create
+    @boat = Boat.new(boat_params)
+      @boat.save
+
+    # redirect_to boats_path
+    # else
+    #   render "new"
+    # end
+  end
+
+
+  private
+
+  def boat_params
+    params.require(:boat).permit(:title, :description, :photo)
+  end
+end
