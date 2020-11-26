46f1e631b1317a85f81f1dad3285c16e793ed9ab
Ludovic Villain <chadludo@gmail.com>
2020-11-26 12:58:37 +0100

    booking form works

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,7 @@
 class BookingsController < ApplicationController
 
   def index
-    @booking = Boooking.all
+    @booking = Booking.all
   end
 
   def new
@@ -9,7 +9,25 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @booking = Boooking.new(booking_params)
-        @booking.save
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.boat = Boat.find(params[:boat_id])
+
+    start = Time.new(params[:booking]["starts_on(1i)"], params[:booking]["starts_on(2i)"], params[:booking]["starts_on(3i)"]).to_i
+
+    @end = Time.new(params[:booking]["ends_on(1i)"], params[:booking]["ends_on(2i)"], params[:booking]["ends_on(3i)"]).to_i
+
+    days = ((@end - @start) / 60000)
+
+    price_per_day = @booking.boat.price_per_day
+    @booking.total_price = days * price_per_day
+    @booking.save
+
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:starts_on, :ends_on)
   end
 end
