f513b9e7467d60b9494cac290a5b8507dd3daf21
lise pluger <lisepluger@gmail.com>
2020-11-27 15:47:29 +0100

    owner-page

--- app/controllers/owner/bookings_controller.rb
+++ app/controllers/owner/bookings_controller.rb
@@ -2,6 +2,24 @@ class Owner::BookingsController < ApplicationController
 
   def index
     @bookings = current_user.owner_bookings
+    @owner = current_user
+    @boats = @owner.boats
+    @reservations = Booking.where(boat: @boats)
+
+
+  end
+
+  def accept
+   @booking = Booking.find(params[:id])
+   @booking.status = "accepted"
+   @booking.save!
+
+  end
+
+  def decline
+    @booking = Booking.find(params(:id))
+   @booking.status = "refused"
+   @booking.save!
   end
 
 end
